2b93405a4d2a35caff2751f636b30d3f7c624d63
Lucas <lucasclaudinocc@gmail.com>
2023-11-02 16:58:00 -0300

    Testing ifelse statements

--- Program.cs
+++ Program.cs
@@ -1,4 +1,5 @@
-﻿using FirstApp;
+﻿using System.Linq.Expressions;
+using FirstApp;
 
 // Don't forget to use the M after a decimal number
 decimal n1 = 25.32M;
@@ -29,3 +30,13 @@ for (int i = 1; i <= 3; i++){
 // Using classes
 var pessoa = new Pessoa("Lucas", 1300);
 Console.WriteLine($"Nome: {pessoa.Nome}, Saldo: {pessoa.Saldo}");
+
+// Brazilian Portuguese
+var userChoice = Console.ReadLine();
+
+if (userChoice!.Length > 10)
+{
+    Console.WriteLine("Você digitou mais de 10 caracteres");
+} else {
+    Console.WriteLine("Você digitou menos de 10 caracteres");
+}
