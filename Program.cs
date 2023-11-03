393c89a4ef0be2d3ba54ab3245dbe95db74655dd
Lucas <lucasclaudinocc@gmail.com>
2023-11-03 14:36:50 -0300

    Learning how to work with methods in C#

--- Program.cs
+++ Program.cs
@@ -1,42 +1,24 @@
-﻿using System.Linq.Expressions;
-using FirstApp;
+﻿using FirstApp;
 
-// Don't forget to use the M after a decimal number
-decimal n1 = 25.32M;
-int n2 = 14;
+string myName = Console.ReadLine()!;
 
-// String representation
-string hey = "Hello, my name is Lucas";
-string credentials = "I'm a web developer";
-
-// Boolean representation
-bool isItCool = false;
-
-// String Concatenation
-if (isItCool == false)
-Console.WriteLine(n1 + n2);
-// Console.WriteLine(string.Format("{0} - {0} - {1}", hey, credentials));
-Console.WriteLine($"{hey}. {credentials}");
-
-// var valorDoSaque2 = 200;
-int valor = 200;
-
-// For - Estrutura de repeticao
-for (int i = 1; i <= 3; i++){
-    Console.WriteLine($"Mês {i}: {valor}");
-    valor += 200;
-}
-
-// Using classes
-var pessoa = new Pessoa("Lucas", 1300);
-Console.WriteLine($"Nome: {pessoa.Nome}, Saldo: {pessoa.Saldo}");
-
-// Brazilian Portuguese
-var userChoice = Console.ReadLine();
-
-if (userChoice!.Length > 10)
+bool isMyNameLucas (string name)
 {
-    Console.WriteLine("Você digitou mais de 10 caracteres");
-} else {
-    Console.WriteLine("Você digitou menos de 10 caracteres");
+    return name == "Lucas";
 }
+
+void printName (string name)
+{
+    Console.WriteLine(name);
+}
+
+if (isMyNameLucas(myName))
+{
+    Console.WriteLine("Hey, Lucas!");
+    printName(myName);
+}
+else
+{
+    Console.WriteLine("Not Lucas");
+}
+
